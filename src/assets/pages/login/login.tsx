6eb6b127b20c8d74c89d1c1890aedcc247983311
WillSimao <williamsimaocavalcante75@gmail.com>
2023-10-17 21:54:47 -0300

    Finaliza desktop

--- src/assets/pages/login/login.tsx
+++ src/assets/pages/login/login.tsx
@@ -10,23 +10,21 @@ const Login = () => {
                 <img className={styles['img']} src={backgroundLogin} alt="Imagem de login" />
             </div>
             <div className={styles['container-login']} >
-                <div className={styles['container-title']}>
-                    <span className={styles['title-span']} >Bem vindo de voltar</span>
-                    <h1 className={styles['title-h1']}>Faça login na sua conta</h1>
-                </div>
                 <form className={styles['form']} >
+                    <div className={styles['container-title']}>
+                        <span className={styles['title-span']} >Bem vindo de voltar</span>
+                        <h1 className={styles['title-h1']}>Faça login na sua conta</h1>
+                    </div>
                     <label className={styles['form-label']} htmlFor='e-mail'>E-mail</label>
                     <input className={styles['form-input']} type="e-mail" id='e-mail'/>
                     <label className={styles['form-label']} htmlFor="senha">Senha</label>
                     <input className={styles['form-input']} type="password" id="senha" />
 
                     <div className={styles['container-checkbox']} >
-                        <div className={styles['wrapper-checkbox']} >
-                            <div className={styles['input-checkbox']}>
-                                <input name="Lembre de mim" id='checkbox' type="checkbox"/>
-                            </div>
-                            <label className={styles['label-checkbox']} htmlFor="checkbox">Lembre de mim</label>
-                        </div>
+                        <label className={styles['label-checkbox']} htmlFor="checkbox">
+                            <input name="Lembre de mim" id='checkbox' type="checkbox"/>
+                            Lembre de mim
+                        </label>
                         <a className={styles['esqueceu-sua-senha']} href="/">Esqueceu sua senha?</a>
                     </div>
                     <div className={styles['container-button']}>
@@ -37,6 +35,9 @@ const Login = () => {
                         </button>
                     </div>
                 </form>
+
+                <span className={styles['span-cadastro']}>Não tem uma conta? <a href="/">Cadastre-se</a></span>
+
             </div>
         </main>
     )
